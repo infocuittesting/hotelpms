2ad277efffc9839b5834873e99d62ad3090e8b16
PerumalKP <kanperu4@gmail.com>
2018-07-14 12:41:59 +0530

    businessblock

---
+++ Hotel/src/app/businessblock/businessblock.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-businessblock',
+  templateUrl: './businessblock.component.html',
+  styleUrls: ['./businessblock.component.css']
+})
+export class BusinessblockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
